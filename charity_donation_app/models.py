ec30b97dae57d4bf0c9fd63fec3eba0a7035c850
Tomasz Izdebski <lukas4096@gmail.com>
2022-11-27 15:30:20 +0100

    modele--zrobione

--- charity_donation_app/models.py
+++ charity_donation_app/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} "
+
+
+CHOICES = (
+    (1, "fundacja"),
+    (2, "organizacja pozarządowa"),
+    (3, "zbiórka lokalna")
+)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=200, blank=True, null=True)
+    description = models.CharField(max_length=200, blank=True, null=True)
+    type = models.IntegerField(choices=CHOICES, default=1)
+    categories = models.ManyToManyField(Category, related_name="institutions")
+
+    def __str__(self):
+        return f"{self.name} "
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category, related_name="donations")
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=200, blank=True, null=True)
+    phone_number = models.CharField(max_length=200, blank=True, null=True)
+    city = models.CharField(max_length=200, blank=True, null=True)
+    zip_code = models.CharField(max_length=200, blank=True, null=True)
+    pick_up_date = models.DateTimeField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.CharField(max_length=200, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
